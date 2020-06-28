8960f738efaeab2f6a92dac6e8178e78a3ca8291
Leidhe <moroma.monica@gmail.com>
2020-06-29 01:09:00 +0200

    F

--- LearningFast/urls.py
+++ LearningFast/urls.py
@@ -25,5 +25,5 @@ urlpatterns = [
     path('', include('courses.urls')),
     path('accounts/', include('allauth.urls')),
     path('ckeditor/', include('ckeditor_uploader.urls')),
-]+= static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
